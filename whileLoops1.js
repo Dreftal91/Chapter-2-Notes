968300ddb90cce366a76839a4b6e10f15be8aa74
ixmo <ixmo@fresnou.org>
2023-02-17 14:15:25 -0800

    initial commit

---
+++ whileLoops1.js
@@ -0,0 +1,53 @@
+const readLine = require('readline-sync');
+
+//number 1
+
+// let a = 0;
+// while (a < 26){
+//     console.log(a);
+//     a++;
+// }
+
+//number 2
+
+// let b = 0;
+// while (b < 51){
+//     console.log(b);
+//     b++;
+// }
+
+//number 3
+
+// let c = -10;
+// while (c < 11){
+//     console.log(c);
+//     c++;
+// }
+
+//number 4
+
+// let d = 0;
+// while (d > -51){
+//     console.log(d);
+//     d--;
+// }
+
+//number 5
+
+// let e = 0;
+// while (e < 101){
+//     if (e % 2 == 0){
+//         console.log(e);
+//     }
+//     e++;
+// }
+
+//number 6
+
+let f = 100;
+while (f > 1){
+    if (f % 1 == 0){
+        console.log(f);
+    }
+    f=f-2;
+}
