32a11eb756fcec7c7bb740bc2740489bfae86bc6
ixmo <ixmo@fresnou.org>
2023-02-23 14:17:31 -0800

    commit

---
+++ forLoops1.js
@@ -0,0 +1,73 @@
+const readLine = require('readline-sync');
+
+//number 1
+// for (let i = 0; i <= 150; i++){
+//     console.log(i)
+// }
+
+//number 2
+// for (let i = 0; i <= 40; i++){
+//     console.log(i*i)
+// }
+
+//number 3
+// for (let i = 0; i <= 30; i++){
+//     console.log(i*i*i)
+// }
+
+//number 4
+// for (let i = 0; i <= 700; i++){
+//     if (i % 5 == 0) {
+//         console.log(i)
+//     }
+// }
+
+//number 5
+// for (let i = 350; i <= 1000; i++){
+//     if (i % 10 == 0) {
+//         console.log(i)
+//     }
+// }
+
+//number 6
+// let sum = 0;
+// for (let i = 0; i <= 50; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//number 7
+// let sum = 35;
+// for (let i = 35; i <= 70; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//number 8
+// let sum = 1;
+// for (let i = 1; i <= 15; i++){
+//     sum *= i;
+// }
+// console.log(sum);
+
+//number 9
+// let sum = 10;
+// for (let i = 10; i <= 35; i++){
+//     sum *= i;
+// }
+// console.log(sum);
+
+// number 10
+// for (let i = 1; i <= 12; i++){
+//     console.log("Happy Holidays!")
+// }
+
+//number 11
+// let num = readLine.question("How many times do you want to print?: ");
+// for (let i = 1; i <= num; i++){
+//     if (1 <= num){
+//         console.log("Happy Holidays!")
+//     }
+// }
+
+//number 12
